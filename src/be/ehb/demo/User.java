f28cd683e0095f8c8cdad39e62c68ec50586043b
David-VS <63110701+David-VS@users.noreply.github.com>
2025-03-28 08:53:47 +0100

    Added boilerplate methods to class user

--- src/be/ehb/demo/User.java
+++ src/be/ehb/demo/User.java
@@ -1,4 +1,48 @@
 package be.ehb.demo;
 
 public class User {
+
+    private String email, firstname, lastname, password;
+
+    public User() {
+    }
+
+    public User(String email, String firstname, String lastname, String password) {
+        this.email = email;
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
